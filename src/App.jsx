ed355a13aec6e03439e0540e82125b16ff3a5409
Matheus Tamine <matheus.tamine@gmail.com>
2025-05-13 17:04:44 -0300

    alterações no app, home e authcontext

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {onAuthStateChanged} from 'firebase/auth'
+import {useState, useEffect, use} from 'react'
+import {AuthProvider} from './components/context/AuthContext.jsx'
+import Navbar from "./components/Navbar.jsx";
+import Footer from './components/Footer'
+import Home from './pages/Home/Home.jsx'
+import Login from './pages/Login/Login.jsx'
+import Register from './pages/Register/Register.jsx'
 import './App.css'
-import Navbar from'./components/Navbar'
-import  Footer from './components/Footer'
-import Home from './pages/Home/Home'
+import { useAuthentication } from "./hooks/useAuthentication.jsx";
 
 function App() {
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
 
+  const loadingUser = user == undefined
+  useEffect(() => {
+    onAuthStateChanged(auth, () => {
+       setUser(user)
+    })
+   
+  }, [auth])
+
+  if (loadingUser) {
+    return <p>Carregando a página</p>
+  }
+
+  
   return (
     <>  
-    <Navbar />    
-    <Home />
-    <Footer />
-
+      <div>
+        <AuthProvider value={user}>
+          <BrowserRouter>
+          <Navbar />    
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home/>} /> 
+              <Route path="/login" element={<Login/>} /> 
+              <Route path="/register" element={<Register/>} /> 
+            </Routes>
+          </div>
+          <Footer />
+          </BrowserRouter>
+        </AuthProvider>
+      </div>
     </>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,12 @@
 import style from './Home.module.css'
 
+import {useFetchDocuments} from '../../hooks/useFetchDocuments'
+import {useNavigate} from 'react-router-dom'
+
 const Home = () => {
   return (
     <>
-    <h1>Veja os posts mais recentes</h1>
-    <form className={style.search_form}>
-        <input 
-        type="text" 
-        placeholder='Ou busque por tags...'
-        />
-        <button className="btn btn-dark">Pesquisar</button>
-    </form>
+    
     </>
   )
 }
