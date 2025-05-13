0a0787a1178a4e7fa5f7d914c4b9f99574367dce
VitorBZS <bonilhazanattasilva@gmail.com>
2025-05-13 18:40:41 -0300

    Update config.jsx
    Erro, em getFireStore
    Correto: getFirestore

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
-import{ getFireStore } from "firebase/firestore"
+import{ getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyD_4y-nK5V3nag6KyNy9KvHfVuX-b4n3h0",
